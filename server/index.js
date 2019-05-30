1f45c140a93f30cf54eafc77b0eaff861da186c4
manufosela <manufosela@gmail.com>
2019-05-30 14:29:10 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const db = {
+  "categories": ["kairos", "laboral", "desarrollo", "agile"],
+  "resources": {
+    "kairos":[
+      {
+        "title": "calendario laboral",
+        "description": "Calendario laboral del año en curso",
+        "masinfo": "",
+        "url": "https://www.calendarioslaborales.com/calendario-laboral-madrid-2019.htm"
+      },
+      {
+        "title": "",
+        "description": "",
+        "masinfo": "",
+        "url": ""
+      },
+      {
+        "title": "",
+        "description": "",
+        "masinfo": "",
+        "url": ""
+      }
+    ],
+    "laboral": [
+      {
+        "title": "",
+        "description": "",
+        "masinfo": "",
+        "url": ""
+      }
+    ],
+    "desarrollo": [
+      {
+        "title": "",
+        "description": "",
+        "masinfo": "",
+        "url": ""
+      }
+    ],
+    "agile": [
+      {
+        "title": "",
+        "description": "",
+        "masinfo": "",
+        "url": ""
+      }
+    ]
+  }
+}
+
+app.get('/', function (req, res) {
+	res.json({ "kairectorio": "API REST" });
+});
+
+app.get('/categories', function (req, res) {
+  let result = db.categories;
+	res.json(result);
+});
+
+app.get('/resources/:category', function (req, res) {
+  let category = req.params.category
+  let result = db.resources[category];
+	res.json(result);
+});
+
+app.listen(PORT, function () {
+	console.log('Your node js server is running on PORT:', PORT);
+});
