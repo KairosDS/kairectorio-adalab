6920dbc7ad52bb659b385af0570c67c3f08e751c
manufosela <manufosela@gmail.com>
2019-06-03 09:40:04 +0200

    add db resources

--- server/index.js
+++ server/index.js
@@ -5,7 +5,33 @@ const PORT = 3000;
 const db = {
   "categories": ["kairos", "laboral", "desarrollo", "agile"],
   "resources": {
-    "kairos":[
+    "kairos":[    
+      {
+        "title": "Kairos Digital Solutions",
+        "description": "Kairós Digital Solutions web",
+        "masinfo": "",
+        "url": "https://www.kairosds.com"
+      },
+      {
+        "title": "KairósDS en Linkedin",
+        "description": "KairósDS en Linkedin",
+        "masinfo": "",
+        "url": "https://www.linkedin.com/company/kair%C3%B3s-digital-solutions-s-l-"
+      },
+      {
+        "title": "KairósDS en Twitter",
+        "description": "KairósDS en Twitter",
+        "masinfo": "",
+        "url": "https://twitter.com/kairos_ds"
+      },
+      {
+        "title": "KairósDS en Facebook",
+        "description": "KairósDS en Facebook",
+        "masinfo": "",
+        "url": "https://www.facebook.com/kariros.digital.solutions/"
+      }
+    ],
+    "laboral": [
       {
         "title": "calendario laboral",
         "description": "Calendario laboral del año en curso",
@@ -13,19 +39,11 @@ const db = {
         "url": "https://www.calendarioslaborales.com/calendario-laboral-madrid-2019.htm"
       },
       {
-        "title": "",
-        "description": "",
+        "title": "Convenio Colectivo",
+        "description": "Convenio Colectivo de consultoria",
         "masinfo": "",
-        "url": ""
+        "url": "https://www.boe.es/boe/dias/2018/03/06/pdfs/BOE-A-2018-3156.pdf"
       },
-      {
-        "title": "",
-        "description": "",
-        "masinfo": "",
-        "url": ""
-      }
-    ],
-    "laboral": [
       {
         "title": "",
         "description": "",
@@ -35,22 +53,64 @@ const db = {
     ],
     "desarrollo": [
       {
-        "title": "",
-        "description": "",
+        "title": "Guia de Javascript",
+        "description": "Guia de Javascript de MDN",
         "masinfo": "",
-        "url": ""
+        "url": "https://developer.mozilla.org/es/docs/Web/JavaScript/Guide"
+      },
+      {
+        "title": "Guía de Lit-Element",
+        "description": "Guía de Lit-Element de Google",
+        "masinfo": "",
+        "url": "https://lit-element.polymer-project.org/guide"
+      },
+      {
+        "title": "Angular. Getting Started",
+        "description": "Primeros pasos con Angular 8",
+        "masinfo": "",
+        "url": "https://angular.io/start"
+      },
+      {
+        "title": "React. Tutorial",
+        "description": "Tutorial de React",
+        "masinfo": "",
+        "url": "https://reactjs.org/tutorial/tutorial.html"
+      },
+      {
+        "title": "VueJS. Getting Started",
+        "description": "Primeros pasos con VueJS 2",
+        "masinfo": "",
+        "url": "https://vuejs.org/v2/guide/"
       }
     ],
     "agile": [
       {
-        "title": "",
-        "description": "",
+        "title": "Manifiesto Ágil",
+        "description": "Manifiesto Agil",
         "masinfo": "",
-        "url": ""
+        "url": "https://agilemanifesto.org/iso/es/manifesto.html"
+      },
+      {
+        "title": "Kanban - TKP",
+        "description": "Certificate en Kanban - TKP",
+        "masinfo": "",
+        "url": "http://lku.kairosds.com/"
+      },
+      {
+        "title": "Management 3.0",
+        "description": "Redefining leadership: managing the system, not the people",
+        "masinfo": "",
+        "url": "https://management30.com/"
+      },
+      {
+        "title": "Lean Change Management",
+        "description": "Lean Change Management is a feedback-driven approach to change management inspired by combining the best ideas from Agile, Lean Startup, Change Management and Design Thinking.",
+        "masinfo": "",
+        "url": "https://leanchange.org/"
       }
     ]
   }
-}
+};
 
 app.get('/', function (req, res) {
 	res.json({ "kairectorio": "API REST" });
