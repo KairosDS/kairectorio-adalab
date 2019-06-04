85d7f42754d2cc07ea5302d4b1cc028c1c31f718
wczarnomysy <w.czarnomysy@gmail.com>
2019-06-04 16:03:49 +0200

    Button back

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,24 +4,32 @@ import CardDetail from "../CardDetail/CardDetail";
 import BackButton from "../BackButton/BackButton";
 
 class Card extends React.Component {
-  render () {
-    const {kairos, laboral, desarrollo, agile, match, chosenCategory, fetchResources} = this.props;
+  render() {
+    const {
+      kairos,
+      laboral,
+      desarrollo,
+      agile,
+      match,
+      chosenCategory,
+      fetchResources
+    } = this.props;
     const id = match.params.id;
-    
-    const chooseCategory = (id) => {
-      if(id === 'kairos') {
+
+    const chooseCategory = id => {
+      if (id === "kairos") {
         return kairos;
-      } else if (id === 'laboral'){
+      } else if (id === "laboral") {
         return laboral;
-      } else if (id === 'desarrollo'){
+      } else if (id === "desarrollo") {
         return desarrollo;
       } else {
         return agile;
       }
-    }
+    };
 
-    const isCategory = (chosenCategory) => {
-      if(chosenCategory !== '') {
+    const isCategory = chosenCategory => {
+      if (chosenCategory !== "") {
         fetchResources(chosenCategory);
         const array = chooseCategory(chosenCategory);
         return array;
@@ -29,32 +37,30 @@ class Card extends React.Component {
         const array = [];
         return array;
       }
-    }
+    };
 
     const array = isCategory(chosenCategory);
 
     return (
-      <div className="main__directory--wrapper">
-        {array.length > 0 ?
-          array
-          .filter(item => (item.title.includes(id) ? item : false))
-          .map((item, index) => {
-            return (
-              <div className="card__container--final" key={index}>
-                <h3 className="card__title">{item.title}</h3>
-                <CardDetail
-                  description={item.description}
-                  url={item.url}
-                />
-              </div>
-            );
-          })
-        :
-        <p>No hay datos</p>
-        }
-         <BackButton route={`/resources/${chosenCategory}`} />
-
-      </div>
+      <React.Component>
+        <div className="main__directory--wrapper">
+          {array.length > 0 ? (
+            array
+              .filter(item => (item.title.includes(id) ? item : false))
+              .map((item, index) => {
+                return (
+                  <div className="card__container--final" key={index}>
+                    <h3 className="card__title">{item.title}</h3>
+                    <CardDetail description={item.description} url={item.url} />
+                  </div>
+                );
+              })
+          ) : (
+            <p>No hay datos</p>
+          )}
+        </div>
+        <BackButton route={`/resources/${chosenCategory}`} />
+      </React.Component>
     );
   }
 }
