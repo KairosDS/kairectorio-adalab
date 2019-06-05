3338ded3f53bb84a87fe8b3dd962e5e9f7c4dc21
superanika <anamaria.romerogarcia@gmail.com>
2019-06-05 13:08:14 +0200

    create docs

---
+++ docs/precache-manifest.09224bf01bec93ab19b3acc584c0dec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f26f7ff62ecaba8822fab491dfa5a09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f312af97f932bba97e5c",
+    "url": "./static/css/main.52e495e6.chunk.css"
+  },
+  {
+    "revision": "bf3696fa62ef995165a8",
+    "url": "./static/js/2.8f61be19.chunk.js"
+  },
+  {
+    "revision": "f312af97f932bba97e5c",
+    "url": "./static/js/main.dbbdf5d9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3c596e099ad4f4b7c79bafe69b4d5081",
+    "url": "./static/media/headerBgn.3c596e09.jpg"
+  },
+  {
+    "revision": "0479f453fe035a5d24acb348c4c34179",
+    "url": "./static/media/logonaranja.0479f453.jpg"
+  }
+]);
